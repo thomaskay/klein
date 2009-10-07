dcfbc68e362719f5d85139bd7314909036ab1767
Thomas Kristiansen <thomaskay@unwire-275.local>
2009-10-07 23:48:21 +0200

    adding tests

---
+++ test/klein_test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+require 'rubygems'
+require 'active_record'
+
+
+
+class KleinTest < Test::Unit::TestCase
+  # Replace this with your real tests.
+  def test_this_plugin
+    flunk
+  end
+end
