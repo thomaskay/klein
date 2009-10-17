b67ea912fa7755d1bb7444c638cc3c91395d0622
Thomas Kristiansen <thomaskay@unwire-275.local>
2009-10-17 23:07:51 +0200

    handle arrays and symbols

--- lib/translation.rb
+++ lib/translation.rb
@@ -17,7 +17,14 @@ module I18n
       
       def hasher(path)
         if path.length == 0
-          return {name => text}
+          if(text.first == '[' && text.last == ']' ) #handle arrays
+            evaluated = text[1..-2].split(',').collect!{|n|n.strip}
+            evaluated.collect!{|n| (n.first==":" ? n.slice(/[^:].*/).to_sym : n  )} #handle symbols
+          else
+            evaluated = text
+          end
+
+          return {name => evaluated}
         end
         hash = {}        
         hash[path.first] = hasher(path.slice(1..-1) )
@@ -33,9 +40,11 @@ module I18n
           end
         else
           #value is not a hash, but an actual translation
-          obj = self.find_by_key(key, :conditions => ["locale=?", locale]) #find existing
-          obj = new({:locale=>locale,:key=>key,:text=>value}) if obj.nil? #or use new
-          
+          if value.instance_of?Array
+            value = "[#{value.map{|v|(v.instance_of?Symbol)?":#{v.to_s}":v.to_s}.join(',')}]";
+          end
+          obj = self.find_or_create_by_key(key, :locale => locale)
+          obj.locale = locale 
           obj.text = value
           obj.save
         end
