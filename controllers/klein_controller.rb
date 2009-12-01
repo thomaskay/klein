c6192477dfd165ec2f7cdcadffa7fb24c6ba1cb5
Thomas Kristiansen <thomaskay@unwire-275.local>
2009-12-01 21:17:24 +0100

    gui

--- controllers/klein_controller.rb
+++ controllers/klein_controller.rb
@@ -4,7 +4,18 @@ class KleinController < ActionController::Base
 
 
   def index
+    @total_translation_keys = I18n::Backend::Translation.count(:key,:distinct=>true)
+    
 #render :text => "Squawk!"
+  end
+  
+  def translate
+    
+  end
+
+  def filter
+    
+    
   end
 
 
--- lib/routing.rb
+++ lib/routing.rb
@@ -2,7 +2,9 @@ module Klein #:nodoc:
   module Routing #:nodoc:
     module MapperExtensions
       def klein
-        @set.add_route("/klein", {:controller => "klein", :action => "index"})
+        @set.add_route("/klein/translate", {:controller => "klein", :action => "translate"})        
+        @set.add_route("/klein/filter", {:controller => "klein", :action => "filter"})        
+        @set.add_route("/klein", {:controller => "klein", :action => "index"})        
       end
     end
   end
